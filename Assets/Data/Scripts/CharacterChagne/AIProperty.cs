eb5a5ba38e8aadb9fd8e819374e1df9b77d88642
M3314 <srapid670@gmail.com>
2022-07-20 12:33:52 +0900

    AIproperty 수정
    AIproperty 수정
    
    잘 안되네 이거가 잘 모르겠고 의도가 뭐냐면 범위내에 들어오면 공격하게 하는건데 잘 안됨 ㅇㅅㅇ

--- Assets/Data/Scripts/CharacterChagne/AIProperty.cs
+++ Assets/Data/Scripts/CharacterChagne/AIProperty.cs
@@ -1,43 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
+using UnityEngine.Events;
 
 public class AIProperty : MonoBehaviour
 {
-    Animator _anim = null;
-    protected Animator myAnim
-    {
-        get
-        {
-            if(_anim == null)
-            {
-                _anim = this.GetComponentInChildren<Animator>();
-            }
-            return _anim;
-        }
-    }
-    [SerializeField] EnemyData enemysdata = null;
-    protected EnemyData myData
-    {
-        get => enemysdata;
-    }
-
-   float? CurHP = null;
-
-    protected bool UpdateHP(float data)
-    {
-        if(CurHP == null)
-        {
-           CurHP = enemysdata.GetMaxHP();
-        }
-        CurHP += data;
-        if(CurHP < 0.0f)
-        {
-            CurHP = 0.0f;
-            return false;
-        }
-        return true;
-    }
+ 
 
 }
